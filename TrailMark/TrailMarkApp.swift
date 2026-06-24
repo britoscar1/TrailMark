af37819f9dcf6f8e406262a859bb1bb4bcbda3ce
britoscar1 <brito.oscar@uabc.edu.nx>
2026-06-23 17:24:29 -0700

    Initial Commit

---
+++ TrailMark/TrailMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailMarkApp.swift
+//  TrailMark
+//
+//  Created by Oscar Artemio Brito Ortiz on 23/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TrailMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
